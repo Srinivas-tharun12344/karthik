01e1c1170fbd55303b9d72624ee71e3f834fa5a2
Srinivas-tharun12344 <srinivasyenugu383@gmail.com>
2025-10-17 23:19:00 +0530

    Add full project files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace 'karthik' with your repo name
 export default defineConfig({
-    base: '/karthik/',    
+  base: '/karthik/',    
   plugins: [react()],
 })
