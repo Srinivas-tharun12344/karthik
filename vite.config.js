5c05ab42303d4a13b44929fb55d64f44cc2b25e7
Srinivas-tharun12344 <srinivasyenugu383@gmail.com>
2025-10-17 23:12:19 +0530

    Add full project files

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/karthik/',  // <-- GitHub repo name as the base path
+    base: '/karthik/',    
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
 })
